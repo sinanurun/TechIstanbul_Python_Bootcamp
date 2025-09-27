caf9ccbc2714e33865d7b271df2c23bc0454ac2b
Sinan URUN <sinanurun24@gmail.com>
2025-09-27 16:58:56 +0300

    Refactor dictionary key-value swapping function and enhance usage examples

--- Hafta02/gun06_27Eylul/ders25.py
+++ Hafta02/gun06_27Eylul/ders25.py
@@ -1,6 +1,15 @@
 # Sözlük Anahtar-Değer Değiştirme
 # Bir sözlük içindeki anahtar ve değerleri yer değiştiren fonksiyon
 # Fonksiyon, yeni oluşturulan sözlüğü return ile döndürecek
+"""
+#yontem1
+def anahtar_deger_degistir(sozluk):
+    yeni_sozluk = {}
+    for anahtar, deger in sozluk.items():
+        yeni_sozluk[deger] = anahtar
+    return yeni_sozluk
+"""
+#yontem2
 def anahtar_deger_degistir(sozluk):
     return {deger: anahtar for anahtar, deger in sozluk.items()}
 
@@ -8,3 +17,9 @@ def anahtar_deger_degistir(sozluk):
 renkler = {"kırmızı": "#FF0000", "yeşil": "#00FF00", "mavi": "#0000FF"}
 ters_renkler = anahtar_deger_degistir(renkler)
 print("Ters çevrilmiş:", ters_renkler)
+
+
+treng = {"kırmızı":"red","yeşil":"green","mavi":"blue"}
+
+engtr = anahtar_deger_degistir(treng)
+print("Ters çevrilmiş:", engtr)
