32280270956e3834ebd6edd83b43cb631716c634
sinanurun <sinanurun24@gmail.com>
2025-10-27 05:40:47 +0300

    df

--- Hafta06/gun18_25Ekim/iliskisel_orm/models.py
+++ Hafta06/gun18_25Ekim/iliskisel_orm/models.py
@@ -3,6 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    kitaplar = db.relationship('Kitap', backref='user', lazy=True, cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f'<Kategori {self.ad}>'    
+
 class Kategori(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ad = db.Column(db.String(100), nullable=False, unique=True)
@@ -16,6 +24,7 @@ class Kitap(db.Model):
     baslik = db.Column(db.String(200), nullable=False)
     yazar = db.Column(db.String(100), nullable=False)
     kategori_id = db.Column(db.Integer, db.ForeignKey('kategori.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, default=1)
 
     def __repr__(self):
         return f'<Kitap {self.baslik}>'
